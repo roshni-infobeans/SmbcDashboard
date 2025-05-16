68fbf80d89169274e457ea488836b7b30202489b
roshini.verma <roshni.verma@infobeans.com>
2025-05-16 16:21:34 +0530

    cosmetic changes

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link href="assets/img/logo/logo.png" rel="icon">
+  <!-- <link href="assets/img/logo/logo.png" rel="icon"> -->
   <title>SMBC - Dashboard</title>
   <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
+<?php include 'header.php'; ?>
+<!-- <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -12,20 +12,14 @@
   <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="assets/css/ruang-admin.min.css" rel="stylesheet">
-</head>
+</head> -->
 
 <body id="page-top">
   <div id="wrapper">
     <?php include 'sidebar.php'; ?>
     <div id="content-wrapper" class="d-flex flex-column">
       <div id="content">
-        <!-- TopBar -->
-        <nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top">
-          <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
-        </nav>
-        <!-- Topbar -->
+        <?php include 'topbar.php'; ?>
 
         <!-- Container Fluid-->
         <div class="container-fluid" id="container-wrapper">
